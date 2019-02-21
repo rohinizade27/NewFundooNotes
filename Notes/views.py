31a3d417d39a47e3677c34cf843f937dd44a748a
admin1 <rohinizade43@gmail.com>
2019-02-21 19:39:43 +0530

    uoi update

--- Notes/views.py
+++ Notes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, redirect, render_to_response
 def home(request):
 
     return render(request, 'Notes/note_section.html')
+
+def home1(request):
+
+    return render(request, 'Notes/practice.html')
